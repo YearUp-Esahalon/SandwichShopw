6274b610404281346030cbe46e6a5c97f29c85e2
Elan Sahalon <Esahalon1996@gmail.com>
2024-10-01 10:06:11 -0400

    first commit

---
+++ SandwichShopw/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Sandwich size selection
+        System.out.println(" Select the size of the sandwich: ");
+        System.out.println("(A)Regular: base price $5.45");
+        System.out.println("(B)Large: base price $8.95");
+        String chooseSize = scanner.nextLine();
+
+        float basePrice = 0;
+        if (chooseSize.equals ("A")) {
+            basePrice = 5.45F;
+        } else if (chooseSize.equals("B")) {
+            basePrice = 8.95f;
+        }
+
+        // ask for Age
+        System.out.print( "What is your age? ");
+        int age = scanner.nextInt();
+
+        float discount = 0;
+        if(age <=17) {
+            discount = 0.10F;
+        } else if (age>=65) {
+            discount = 0.20F;
+        }
+
+        //Total price post discount 
+        float totalCost= basePrice - (basePrice * discount);
+
+        // Get a Result
+        System.out.println(" The cost of your sandwich with the discount is: $" + totalCost);
+
+
+    }
+}
